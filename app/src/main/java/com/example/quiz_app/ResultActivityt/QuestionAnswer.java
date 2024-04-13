4e29a761289c5c225149ac526c9c13f1fe19e6a1
Bibha Bindu Mohapatra <bibhabindum@gmail.com>
2024-04-13 22:33:56 +0530

    finished project

---
+++ app/src/main/java/com/example/quiz_app/ResultActivityt/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "What is 30+40 ?",
+            "Who is the CEO of Apple?",
+            "what is 11*11 ?",
+            "who is the CEO of Realme?",
+            "Who is the first person you met in the college"
+    };
+    public static String choices [][]={
+            {"45" , "53" , "70" , "68"},
+            {"Satya Nadella" , "Ratan Tata" , "Tim Cook" , "None of the above"},
+            {"48" , "60" , "121" , "98"},
+            {"Madhav Seth" , "Carl Pei" , "Steve jobs" , "Bill Gates"},
+            {"Prateek" , "Manish" , "Ram" , "Shyam"}
+    };
+    public static String correctAnswers []={
+            "70" ,
+            "Tim Cook",
+            "121",
+            "Madhav Seth",
+            "Prateek"};
+}
